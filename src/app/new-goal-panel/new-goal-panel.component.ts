807412ca083f2152d406fa6ee0e7cf38d8fe9845
Alexander Frize <alexander.frize@gmail.com>
2017-02-23 14:04:21 -0800

    Added upload file button (frontend)

--- src/app/new-goal-panel/new-goal-panel.component.ts
+++ src/app/new-goal-panel/new-goal-panel.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class NewGoalPanelComponent {
+	private filesToUpload : Array<File> = [];
 
+/* ========================= Analyze the file information before upload ========================= */		
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>> fileInput.target.files;
+    console.log(this.filesToUpload[0]);
+  }
 }
